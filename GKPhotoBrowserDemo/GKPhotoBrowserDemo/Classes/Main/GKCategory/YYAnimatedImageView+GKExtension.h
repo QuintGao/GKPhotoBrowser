3ed02ae8e487274059f54831a1f971f796f9034b
QuintGao <1094887059@qq.com>
2020-12-09 09:28:33 +0800

    修复iOS14，使用YYAnimateImageView本地图片不显示的bug

---
+++ GKPhotoBrowserDemo/GKPhotoBrowserDemo/Classes/Main/GKCategory/YYAnimatedImageView+GKExtension.h
@@ -0,0 +1,17 @@
+//
+//  YYAnimatedImageView+GKExtension.h
+//  GKPhotoBrowserDemo
+//
+//  Created by gaokun on 2020/12/9.
+//  Copyright © 2020 QuintGao. All rights reserved.
+//
+
+#import <YYImage/YYImage.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YYAnimatedImageView (GKExtension)
+
+@end
+
+NS_ASSUME_NONNULL_END
